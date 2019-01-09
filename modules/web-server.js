d576d7f0b1df8baf6d51fcceb082a083cda663cb
temikng <temikng@gmail.com>
2019-01-09 19:44:28 +0500

    Added check user attestation to web-server

--- modules/web-server.js
+++ modules/web-server.js
@@ -18,6 +18,12 @@ function startWebServer() {
 
 	app.get('/:bbAddress', (req, res) => {
 		const { params, cookies } = req;
+		const { bbAddress } = params;
+
+		if (!validationUtils.isValidAddress(bbAddress)) {
+			return responseRedirect(res);
+		}
+
 		db.query(
 			`SELECT
 				device_address, user_address, users.bt_user_id,
@@ -25,77 +31,105 @@ function startWebServer() {
 			FROM users
 			LEFT JOIN receiving_addresses USING(device_address, user_address)
 			WHERE user_address=?`,
-			[params.bbAddress],
+			[bbAddress],
 			(rows) => {
 				if (!rows.length) {
 					return responseRedirect(res);
 				}
 
+				const device = require('byteballcore/device.js');
 				const userInfo = rows[0];
 				const { bt_user_id: btUserId, user_address: userAddress, device_address: deviceAddress } = userInfo;
-				if (cookies.referrer && validationUtils.isValidAddress(cookies.referrer)) {
-					db.query(
-						`INSERT ${db.getIgnore()} INTO link_referrals
-						(referring_user_address, device_address, type)
-						VALUES(?, ?, 'cookie')`,
-						[cookies.referrer, deviceAddress],
-					);
-				}
 
-				if (!userAddress) {
-					device.sendMessageToDevice(deviceAddress, 'text', texts.insertMyAddress());
+				if (!btUserId) {
+					device.sendMessageToDevice(deviceAddress, 'text', texts.insertBitcointalkProfileLink());
 					return responseRedirect(res);
 				}
 
-				const device = require('byteballcore/device.js');
-
-				api.getProfileData(btUserId, params.bbAddress)
-					.then((profileData) => {
-						if (!profileData.isLinkCorrect) {
-							return device.sendMessageToDevice(deviceAddress, 'text', "Your bitcointalk profile doesn't contain a correct link");
-						}
-
-						receivingAddresses.readOrAssign(userInfo, (receivingAddress, postPublicly) => {
-							db.query(
-								`UPDATE receiving_addresses
-								SET
-									bt_user_name=?,
-									bt_user_rank=?,
-									bt_user_rank_index=?,
-									bt_user_activity=?,
-									bt_user_posts=?
-								WHERE receiving_address=?`,
-								[
-									profileData.name,
-									profileData.rank,
-									profileData.rankIndex,
-									profileData.activity,
-									profileData.posts,
-									receivingAddress,
-								],
-							);
-	
-							let response = `Your bitcointalk username is ${profileData.name}.\n\n`;
-							const challenge = `${btUserId} ${userAddress}`;
-							if (postPublicly === null) {
-								response += texts.privateOrPublic();
-							} else {
-								response += texts.pleasePay(receivingAddress, conf.priceInBytes, challenge);
-								response += '\n\n';
-								response += (postPublicly === 0)
-									? texts.privateChosen()
-									: texts.publicChosen(profileData.name, btUserId);
+				function checkUserAttestation(onDone) {
+					db.query(
+						`SELECT
+							is_confirmed
+						FROM receiving_addresses
+						JOIN transactions USING(receiving_address)
+						JOIN accepted_payments USING(transaction_id, receiving_address)
+						WHERE receiving_addresses.user_address=?
+							AND receiving_addresses.bt_user_id=?`,
+						[userAddress, btUserId],
+						(rows) => {
+							if (!rows.length) {
+								return onDone(false);
 							}
-							device.sendMessageToDevice(deviceAddress, 'text', response);
-						});
-					})
-					.catch((error) => {
-						console.error(error); // eslint-disable-line no-console
-						notifications.notifyAdmin(`failed getProfileData ${btUserId}`, `${error}, bbAddress: ${params.bbAddress}`);
-						device.sendMessageToDevice(deviceAddress, 'text', 'Failed to get your bitcointalk profile! Please, try later!');
-					});
 
-				responseRedirect(res);
+							const row = rows[0];
+							onDone(!!row.is_confirmed);
+						},
+					);
+				}
+
+				checkUserAttestation((isAttested) => {
+					if (isAttested) {
+						res.cookie('referrer', bbAddress, 3 * 365);
+						return responseRedirect(res);
+					}
+
+					if (cookies.referrer && validationUtils.isValidAddress(cookies.referrer)) {
+						db.query(
+							`INSERT ${db.getIgnore()} INTO link_referrals
+							(referring_user_address, device_address, type)
+							VALUES(?, ?, 'cookie')`,
+							[cookies.referrer, deviceAddress],
+						);
+					}
+
+					api.getProfileData(btUserId, params.bbAddress)
+						.then((profileData) => {
+							if (!profileData.isLinkCorrect) {
+								return device.sendMessageToDevice(deviceAddress, 'text', "Your bitcointalk profile doesn't contain a correct link");
+							}
+
+							receivingAddresses.readOrAssign(userInfo, (receivingAddress, postPublicly) => {
+								db.query(
+									`UPDATE receiving_addresses
+									SET
+										bt_user_name=?,
+										bt_user_rank=?,
+										bt_user_rank_index=?,
+										bt_user_activity=?,
+										bt_user_posts=?
+									WHERE receiving_address=?`,
+									[
+										profileData.name,
+										profileData.rank,
+										profileData.rankIndex,
+										profileData.activity,
+										profileData.posts,
+										receivingAddress,
+									],
+								);
+		
+								let response = `Your bitcointalk username is ${profileData.name}.\n\n`;
+								const challenge = `${btUserId} ${userAddress}`;
+								if (postPublicly === null) {
+									response += texts.privateOrPublic();
+								} else {
+									response += texts.pleasePay(receivingAddress, conf.priceInBytes, challenge);
+									response += '\n\n';
+									response += (postPublicly === 0)
+										? texts.privateChosen()
+										: texts.publicChosen(profileData.name, btUserId);
+								}
+								device.sendMessageToDevice(deviceAddress, 'text', response);
+							});
+						})
+						.catch((error) => {
+							console.error(error); // eslint-disable-line no-console
+							notifications.notifyAdmin(`failed getProfileData ${btUserId}`, `${error}, bbAddress: ${params.bbAddress}`);
+							device.sendMessageToDevice(deviceAddress, 'text', 'Failed to get your bitcointalk profile! Please, try later!');
+						});
+
+					responseRedirect(res);
+				});
 			},
 		);
 	});
