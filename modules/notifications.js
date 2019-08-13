e51c1d5b0eb9a72ca7b7ce367ac973be9adfa599
Tarmo <tarmo888@hotmail.com>
2019-08-13 10:55:18 +0300

    Process.env (#3)
    * process.env
    
    * testnet explorer

--- modules/notifications.js
+++ modules/notifications.js
@@ -1,13 +1,15 @@
+/*jslint node: true */
+'use strict';
 const conf = require('ocore/conf.js');
 const mail = require('ocore/mail.js');
 
 function notifyAdmin(subject, body) {
-	console.log(`notifyAdmin:\n${subject}\n${body}`); // eslint-disable-line no-console
+	console.error('notifyAdmin:\n' + subject + '\n' + body);
 	mail.sendmail({
 		to: conf.admin_email,
 		from: conf.from_email,
-		subject,
-		body,
+		subject: subject,
+		body: body
 	});
 }
 
